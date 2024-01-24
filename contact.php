68d5cc52725ee34ccd64a24091217296dc7f6b46
Madeleine <mgleahy@nic.edu>
2024-01-24 15:37:01 -0800

    started the form

--- contact.php
+++ contact.php
@@ -1,22 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include('./components/header.php'); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/style.css">
-</head>
-
-
+<?php
+if ($_SERVER("REQUEST_METHOD") == "POST") {
+    $name = htmlspecialchars($_POST ['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    echo $name . "I" . $email . ", " . $message;
+}
+?>
 
 <body>
 
 
     <?php include('./components/nav.php'); ?>
     <main>
-        <!-- <h1>Home Page</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, illum? Velit maiores temporibus dignissimos pariatur, reiciendis quaerat magnam placeat eius quia error assumenda nemo officia sequi enim, similique sit itaque?</p> -->
+        
 
     
         <?php include('./components/footer.php'); ?>
