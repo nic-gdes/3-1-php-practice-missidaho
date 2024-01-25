c91bd2c749c374ea5d690dc91c6cf58f003e23ed
Madeleine <mgleahy@nic.edu>
2024-01-24 18:00:59 -0800

    got the form to work

--- contact.php
+++ contact.php
@@ -1,11 +1,11 @@
 <?php include('./components/header.php'); ?>
 
 <?php
-if ($_SERVER("REQUEST_METHOD") == "POST") {
-    $name = htmlspecialchars($_POST ['name']);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
-    echo $name . "I" . $email . ", " . $message;
+    echo $name . " " . $email . "," . $message;
 }
 ?>
 
@@ -13,12 +13,37 @@ if ($_SERVER("REQUEST_METHOD") == "POST") {
 
 
     <?php include('./components/nav.php'); ?>
-    <main>
-        
+    <main class="form_wrapper">
+        <form action="./contact.php" method="POST">
+            <div class="forms">
 
-    
-        <?php include('./components/footer.php'); ?>
+
+                <div class="name_form"> 
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name">
+                </div>
+                <div class="email_form"> 
+                    <label for="email">Email Address</label>
+                    <input type="email" id="email" name="$email" required>
+                </div>
+                <div class="message_form">
+                     <label for="message">Message:</label>
+                    <textarea id="message" name="message" rows="7"></textarea>
+                </div>
+
+
+            </div>
+
+
+            <button input type="submit">Submit</button>
+
+            </div>
+        </form>
+
+
+
     </main>
+    <?php include('./components/footer.php'); ?>
 </body>
 
 </html>
