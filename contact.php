32c901496cff0d514becd68561d38e8ac2a5896c
Madeleine <mgleahy@nic.edu>
2024-01-24 18:50:41 -0800

    finished

--- contact.php
+++ contact.php
@@ -13,29 +13,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     <?php include('./components/nav.php'); ?>
-    <main class="form_wrapper">
+    <main>
         <form action="./contact.php" method="POST">
-            <div class="forms">
+            <div class="form_wrapper">
 
 
-                <div class="name_form"> 
-                    <label for="name">Name</label>
+                <div class="name_form">
+                    <label for="name">
+                        <h2>Name</h2>
+                    </label>
                     <input type="text" id="name" name="name">
                 </div>
-                <div class="email_form"> 
-                    <label for="email">Email Address</label>
+                <div class="email_form">
+                    <label for="email">
+                        <h2>Email Address</h2></label>
                     <input type="email" id="email" name="$email" required>
                 </div>
                 <div class="message_form">
-                     <label for="message">Message:</label>
+                    <label for="message">
+                        <h2>Message:</h2></label>
                     <textarea id="message" name="message" rows="7"></textarea>
                 </div>
 
 
+
+
+
+                <button input type="submit" class="submit_buuton"><h2>Submit</h2></button>
             </div>
-
-
-            <button input type="submit">Submit</button>
 
             </div>
         </form>
